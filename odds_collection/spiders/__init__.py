940abb89ea6e6bb28a22f14ba1725cccb88f51d7
Derek Gliwa <derek.gliwa@gmail.com>
2024-12-19 00:26:38 -0600

    Fetch regular season matchups

--- odds_collection/spiders/__init__.py
+++ odds_collection/spiders/__init__.py
@@ -3,8 +3,9 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 
+from odds_collection.spiders.nba_season_matchups import NbaSeasonMatchupsSpider
 from odds_collection.spiders.sports_book_moneyline import SportsBookMoneylineSpider
 from odds_collection.spiders.sports_book_spread import SportsBookSpreadSpider
 from odds_collection.spiders.sports_book_over_under import SportsBookOverUnderSpider
 
-__all__ = ["SportsBookMoneylineSpider", "SportsBookSpreadSpider", "SportsBookOverUnderSpider"]
+__all__ = ["NbaSeasonMatchupsSpider","SportsBookMoneylineSpider", "SportsBookSpreadSpider", "SportsBookOverUnderSpider"]
